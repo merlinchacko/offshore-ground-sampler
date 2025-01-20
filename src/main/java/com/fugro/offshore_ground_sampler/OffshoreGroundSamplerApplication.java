9b854e960f1cbfd4cad4161ba839f856f78922f0
pathilch <merlin.chacko@webfleet.com>
2025-01-20 15:23:53 +0100

    "Initial commit of Spring Boot starter service"

---
+++ src/main/java/com/fugro/offshore_ground_sampler/OffshoreGroundSamplerApplication.java
@@ -0,0 +1,13 @@
+package com.fugro.offshore_ground_sampler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffshoreGroundSamplerApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(OffshoreGroundSamplerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fugro/offshore_ground_sampler/OffshoreGroundSamplerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fugro.offshore_ground_sampler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffshoreGroundSamplerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
